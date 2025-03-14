cf5cfdf26fc1618d6b44f52390f0afab4fe4a652
yicr <yicr@users.noreply.github.com>
2025-03-14 14:24:43 +0900

    feat!: Major version bump to 1.0.0 - Initial Stable Release

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,12 +7,14 @@ const project = new awscdk.AwsCdkConstructLibrary({
   jsiiVersion: '5.7.x',
   defaultReleaseBranch: 'main',
   name: '@gammarers/aws-stepfunctions-execution-status-change-notification-stack',
+  description: 'This AWS CDK Stack retrieves the status of Step Functions via EventBridge and sends notifications accordingly. Currently, it only detects and notifies when the status of any machine is either "Failed" or "TimeOut."',
+  majorVersion: 1,
   projenrcTs: true,
   repositoryUrl: 'https://github.com/gammarers/aws-stepfunctions-execution-status-change-notification-stack.git',
   deps: [
-    '@gammarers/aws-stepfunctions-execution-status-change-detection-event-rule@^0.1.2',
+    '@gammarers/aws-stepfunctions-execution-status-change-detection-event-rule@^1.0.0',
   ],
-  releaseToNpm: false, // todo:temp
+  releaseToNpm: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
   minNodeVersion: '18.0.0',
   workflowNodeVersion: '22.x',
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AWS Step Functions Execution Status Change Notification Stack
+
+This AWS CDK Stack retrieves the status of Step Functions via EventBridge and sends notifications accordingly. Currently, it only detects and notifies when the status of any machine is either "Failed" or "TimeOut."
+
